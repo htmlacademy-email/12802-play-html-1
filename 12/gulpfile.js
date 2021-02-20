e587f1027cf72c971d52d87284f7b82d197e85a7
Keks <keks@htmlacademy.ru>
2021-02-20 10:13:15 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/gulpfile.js
@@ -0,0 +1,33 @@
+const stream = require('stream');
+const gulp = require('gulp');
+const htmlnano = require('gulp-htmlnano');
+const rename = require('gulp-rename');
+
+function pages() {
+  return stream.pipeline(
+    gulp.src(['*/*.html', '!**/*.min.html']),
+    htmlnano({
+      collapseWhitespace: 'conservative',
+      removeComments: 'safe',
+      removeEmptyAttributes: false,
+      removeAttributeQuotes: false,
+      removeRedundantAttributes: false,
+      removeOptionalTags: false,
+    }),
+    rename(path => {
+      path.basename += '.min';
+    }),
+    gulp.dest('.'),
+    (error) => {
+      if (error) {
+        console.error(error);
+      }
+    }
+  )
+}
+
+pages.description = 'Обработка HTML для писем';
+
+module.exports = {
+  pages
+}
